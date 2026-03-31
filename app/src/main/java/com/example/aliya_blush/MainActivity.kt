d8e61773876546232d50cd4e4431ad7eab194c58
alyrhmaa <aliya24si@mahasiswa.pcr.ac.id>
2026-03-31 15:24:19 +0700

    ini perubahan

--- app/src/main/java/com/example/aliya_blush/MainActivity.kt
+++ app/src/main/java/com/example/aliya_blush/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() { //ini MainActivity
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
